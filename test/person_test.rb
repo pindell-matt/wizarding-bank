af3ff76ad3fe7b01781cff9bc46b714947e9ab07
Matt Pindell <pindell.m@gmail.com>
2016-01-15 10:46:13 -0700

    created initial folders and Person Class / Person Test

---
+++ test/person_test.rb
@@ -0,0 +1,17 @@
+require 'pry'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class PersonTest < Minitest::Test
+
+
+end
+
+#
+# person1 = Person.new("Minerva", 1000)
+#
+# Minerva has been created with 1000 galleons in cash.
+#
+# person2 = Person.new("Luna", 500)
+#
+# Luna has been created with 500 galleons in cash.
